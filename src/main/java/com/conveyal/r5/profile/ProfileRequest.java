822163100d1a1ce2f6b7c96fc3867dfbdc10c62e
Justin Pihony <justin.pihony@gmail.com>
2023-02-28 18:11:50 -0500

    Create native double method to reduce boxing

--- src/main/java/com/conveyal/r5/profile/ProfileRequest.java
+++ src/main/java/com/conveyal/r5/profile/ProfileRequest.java
@@ -66,9 +66,11 @@ public class ProfileRequest implements Serializable, Cloneable {
 
     /** The speed of walking, in meters per second */
     public float  walkSpeed = 1.3f;
+    public double walkSpeedAsDouble = 1.3d;
     
     /** The speed of cycling, in meters per second */
     public float  bikeSpeed = 4f;
+    public double bikeSpeedAsDouble = 4d;
 
     /** maximum level of traffic stress for cycling, 1 - 4 */
     public int bikeTrafficStress = 4;
@@ -79,6 +81,7 @@ public class ProfileRequest implements Serializable, Cloneable {
      * result in longer travel times than expected on long, high-speed blocks. But we tolerate some imprecision at
      * the scale of individual blocks (see conversation at #436)*/
     public float carSpeed = 2.22f; // ~8 km/h
+    public double carSpeedAsDouble = 2.22d; // ~8 km/h
 
     /** Maximum time to reach the destination without using transit in minutes */
     public int    streetTime = 60;
@@ -264,6 +267,24 @@ public class ProfileRequest implements Serializable, Cloneable {
         //fromTime is in seconds and there are 1000 ms in a second
         return  currentDateTime.plusSeconds(fromTime);
     }
+
+     /**
+     * @return the speed at which the given mode will traverse street edges, in meters per second (natively as double to reduce boxing).
+     */
+    @JsonIgnore
+    public double getSpeedForModeAsDouble (StreetMode streetMode) {
+        switch (streetMode) {
+            case WALK:
+                return walkSpeedAsDouble;
+            case BICYCLE:
+                return bikeSpeedAsDouble;
+            case CAR:
+                return carSpeedAsDouble;
+            default:
+                throw new IllegalArgumentException("getSpeedForMode(): Invalid mode " + streetMode);
+        }
+    }
+
 
     /**
      * @return the speed at which the given mode will traverse street edges, in floating point meters per second.
